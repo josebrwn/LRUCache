e5f11598a47452fe1532a92378f436dd86c6a352
Joseph Brown <josebrwn@gmail.com>
2018-07-22 21:16:38 -0700

    Remove ignored files

--- src/com/josephbrown/Main.java
+++ src/com/josephbrown/Main.java
@@ -3,39 +3,39 @@ public class Main {
     public static void main(String[] args) {
 	/*
     an LRU Cache takes a specific maxSize param, e.g. 50K addresses
-    if a key is not found the user performs a  "slow lookup" (sales_tax_lookup)
+    if a key is not found the user performs a  "slow lookup"
     and inserts the new key-value
      */
 
-	LRUCache fast_rate_lookup = new LRUCache(5);
-	String lookupResult = "";
+	LRUCache c = new LRUCache(5);
+	String result = "";
 
     // user performs "slow lookup" and creates a new key value pair
-    fast_rate_lookup.createNode("1", "1 value");
-    fast_rate_lookup.createNode("2", "2 value");
-    fast_rate_lookup.createNode("3", "3 value");
+    c.createNode("1", "1 value");
+    c.createNode("2", "2 value");
+    c.createNode("3", "3 value");
 
-    lookupResult = fast_rate_lookup.getNode("3");
-    System.out.println("lookupResult is: " + lookupResult);
+    result = c.getNode("3");
+    System.out.println("result is: " + result);
 
-    fast_rate_lookup.createNode("1", "1 value"); // test re-insertion
+    c.createNode("1", "1 value"); // test re-insertion
 
-    lookupResult = fast_rate_lookup.getNode("1");
-    System.out.println("lookupResult is: " + lookupResult);
+    result = c.getNode("1");
+    System.out.println("result is: " + result);
 
-    fast_rate_lookup.createNode("4", "4 value");
-    fast_rate_lookup.createNode("5", "5 value"); // size = 5, LRU = 2
+    c.createNode("4", "4 value");
+    c.createNode("5", "5 value"); // size = 5, LRU = 2
 
-    lookupResult = fast_rate_lookup.getNode("2");
-    System.out.println("lookupResult is: " + lookupResult);
+    result = c.getNode("2");
+    System.out.println("result is: " + result);
 
-    fast_rate_lookup.createNode("6", "6 value");
-    fast_rate_lookup.createNode("7", "7 value");
-    fast_rate_lookup.createNode("8", "8 value");
-    fast_rate_lookup.createNode("9", "9 value");
+    c.createNode("6", "6 value");
+    c.createNode("7", "7 value");
+    c.createNode("8", "8 value");
+    c.createNode("9", "9 value");
 
-    lookupResult = fast_rate_lookup.getNode("asdfasdfasdf");
-    System.out.println("lookupResult is: " + lookupResult);
+    result = c.getNode("asdfasdfasdf");
+    System.out.println("result is: " + result);
 
     }
 }
